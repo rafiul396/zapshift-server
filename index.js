910ea03331a5d3f04b13bb99d8c416d231378a82
rafiul from office <rafiulofficial396@gmail.com>
2025-11-22 19:15:04 +0600

    added post functionality and get functionality

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const port = process.env.PORT || 3000;
 app.use(cors())
 app.use(express.json())
 
+// DB_USER_NAME=zap_shiftUSER
+// DB_USER_PASS=PZP1AUNnNEM3VP5U
 const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASS}@cluster0.abvmfph.mongodb.net/?appName=Cluster0`;
 const client = new MongoClient(uri, {
   serverApi: {
@@ -26,6 +28,29 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const db = client.db("zap-parcel")
+    const parcelColl = db.collection("parcels")
+
+    app.get('/parcels', async (req, res) => {
+        const query = {};
+
+        const {number} = req.query;
+        if(number) {
+            query.receivers_Phone = number
+        }
+
+        const cursor = parcelColl.find(query);
+        const result = await cursor.toArray();
+        res.send(result)
+    })
+
+    app.post('/parcels', async (req, res) => {
+        const parcel = req.body;
+        const result = await parcelColl.insertOne(parcel)
+        res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
