09475223d9d55cb7285ccfcace0b1087d8aaa8ea
rafiul from office <rafiulofficial396@gmail.com>
2025-11-23 18:37:09 +0600

    Added delete method

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 3000;
@@ -49,6 +49,14 @@ async function run() {
         const parcel = req.body;
         const result = await parcelColl.insertOne(parcel)
         res.send(result)
+    })
+
+    app.delete('/parcels/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id : new ObjectId(id) }
+
+      const result = await parcelColl.deleteOne(query)
+      res.send(result)
     })
 
     // Send a ping to confirm a successful connection
